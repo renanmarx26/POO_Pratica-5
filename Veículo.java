cac6eb27183088f6a54df4a11c19fbcaafd27553
renanmarx26 <101154828+renanmarx26@users.noreply.github.com>
2024-03-31 17:53:32 -0300

    Veículo

---
+++ Veículo.java
@@ -0,0 +1,38 @@
+public class Veiculo {
+    private String marca;
+    private String modelo;
+    private int ano;
+
+    public Veiculo(String marca, String modelo, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+    public String toString() {
+        return "Veiculo{ Marca do veículo: " + marca  + ", Modelo do veículo: " + modelo + ", Ano do veículo: " + ano + '}';
+    }
+}
